3c4a8843a9bd2e8f69a7bcd318e113f2f2fa8a95
Ash Nawagamuwage <ash.nawagamuwage@kolmeo.com>
2024-06-12 15:39:11 +1000

    Initial migration for sports and positions

---
+++ src/FanDuel.DepthChart.Infrastructure/Migrations/20240612051258_Initial.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FanDuel.DepthChart.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "DepthChart");
+
+            migrationBuilder.CreateTable(
+                name: "Sports",
+                schema: "DepthChart",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sports", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Positions",
+                schema: "DepthChart",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 3, nullable: false),
+                    SportId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Positions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Positions_Sports_SportId",
+                        column: x => x.SportId,
+                        principalSchema: "DepthChart",
+                        principalTable: "Sports",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Positions_SportId",
+                schema: "DepthChart",
+                table: "Positions",
+                column: "SportId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Positions",
+                schema: "DepthChart");
+
+            migrationBuilder.DropTable(
+                name: "Sports",
+                schema: "DepthChart");
+        }
+    }
+}
--- src/FanDuel.DepthChart.Infrastructure/Persistence/DepthChartContext.cs
+++ src/FanDuel.DepthChart.Infrastructure/Persistence/DepthChartContext.cs
@@ -10,8 +10,32 @@ namespace FanDuel.DepthChart.Infrastructure.Persistence
 {
     public class DepthChartContext : DbContext
     {
+        public const string SchemaName = "DepthChart";
+        public const string MigrationTable = "__EFMigrationsHistory";
+
         public DepthChartContext(DbContextOptions<DepthChartContext> options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.HasDefaultSchema(SchemaName);
+
+            modelBuilder.Entity<Sport>()
+                .HasMany(e => e.Positions)
+                .WithOne(e => e.Sport)
+                .HasForeignKey(e => e.SportId)
+                .IsRequired();
+
+            modelBuilder.Entity<Sport>()
+                .Property(e => e.Name)
+                .IsRequired();
+
+            modelBuilder.Entity<Position>()
+                .Property(e => e.Name)
+                .IsRequired();
         }
 
         public DbSet<Sport> Sports { get; set; }
